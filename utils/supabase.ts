d31fc1a0721237663b2be78cf06206ce4d8b4f5d
billyfe83 <bobfe83@gmail.com>
2024-09-13 12:33:30 +0200

    Update

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,9 +3,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Your Supabase URL and API Key
-const supabaseUrl = "http://127.0.0.1:54321"; // Replace with your actual Supabase URL
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
-; // Replace with your actual Supabase API Key
+const supabaseUrl = 'https://your-project-url.supabase.co'; // Replace with your actual Supabase URL
+const supabaseKey = 'your-anon-key'; // Replace with your actual Supabase API Key
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 

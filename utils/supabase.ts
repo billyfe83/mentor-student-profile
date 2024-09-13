6bc751fa1f5635f109af4e0b9a6f8c38b25aefea
billyfe83 <bobfe83@gmail.com>
2024-09-13 12:02:12 +0200

    second intials

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,14 +1,11 @@
-import { createClient } from '@supabase/supabase-js'
+// utils/supabase.ts
 
-const supabaseUrl = process.env.SUPABASE_URL || ""
-const supabaseKey = process.env.SUPABASE_KEY || ""
+import { createClient } from '@supabase/supabase-js';
 
+// Your Supabase URL and API Key
+const supabaseUrl = "http://127.0.0.1:54321"; // Replace with your actual Supabase URL
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
+; // Replace with your actual Supabase API Key
 
-export default defineNuxtPlugin(() => {
-  const supabase = createClient(supabaseUrl, supabaseKey)
-  return {
-    provide: {
-      supabase
-    }
-  }
-})
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
